9883a0d996591c3e3cd2e459228e0a9feebde5a6
razannael <nailrazan40@gmail.com>
2024-05-14 15:16:53 +0300

    App Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Canvas>
+        <OrbitControls/>
+        <ambientLight intensity={0.5} color={0xffffff}/>
+        <directionalLight position={[0, 0, 5]} color={0xffffff}/>
+        <mesh>
+          <boxGeometry/>
+          <meshStandardMaterial color={'red'}/>
+        </mesh>
+      </Canvas>
     </>
   )
 }
