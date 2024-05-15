3d4b8ca3ab9dd08777405323bbae4ae0bb8cb218
razannael <nailrazan40@gmail.com>
2024-05-15 23:06:05 +0300

    Ground

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,29 @@ import { OrbitControls } from '@react-three/drei'
 
 function App() {
 
+ const Floor = () => {
+   return(
+    <mesh rotation={[-Math.PI * 0.5, 0, 0]}>
+      <planeGeometry args={[20, 20]}/>
+      <meshStandardMaterial color={'#95AC78'} />
+    </mesh>
+   )
+ }
+
   return (
     <>
-      <Canvas>
+      <Canvas 
+      camera={{ position: [0, 5, 15],
+        near: 0.1,
+        far: 100,
+      }}>
         <OrbitControls/>
-        <ambientLight intensity={0.5} color={0xffffff}/>
-        <directionalLight position={[0, 0, 5]} color={0xffffff}/>
-        <mesh>
-          <boxGeometry/>
-          <meshStandardMaterial color={'red'}/>
-        </mesh>
+        <ambientLight intensity={0.1} color={'#b9d5ff'}/>
+        <directionalLight 
+        intensity={1.2}
+        position={[-4, 5, 2]} 
+        color={'#b9d5ff'}/>
+        <Floor/>
       </Canvas>
     </>
   )
